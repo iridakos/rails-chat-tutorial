2a04d7d73a5161903d99e5a656b6b12028a75d13
iridakos <9477868+iridakos@users.noreply.github.com>
2019-04-04 22:35:03 +0300

    Finalize

---
+++ app/assets/javascripts/channels/room_channel.js
@@ -0,0 +1,26 @@
+$(function() {
+  $('[data-channel-subscribe="room"]').each(function(index, element) {
+    var $element = $(element),
+        room_id = $element.data('room-id')
+        messageTemplate = $('[data-role="message-template"]');
+
+    $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000)        
+
+    App.cable.subscriptions.create(
+      {
+        channel: "RoomChannel",
+        room: room_id
+      },
+      {
+        received: function(data) {
+          var content = messageTemplate.children().clone(true, true);
+          content.find('[data-role="user-avatar"]').attr('src', data.user_avatar_url);
+          content.find('[data-role="message-text"]').text(data.message);
+          content.find('[data-role="message-date"]').text(data.updated_at);
+          $element.append(content);
+          $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000);
+        }
+      }
+    );
+  });
+});
