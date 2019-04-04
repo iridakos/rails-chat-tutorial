e1f2f225f885d2be8d579de3b16e989ad9709aa9
iridakos <9477868+iridakos@users.noreply.github.com>
2019-04-04 18:15:05 +0300

    Room management

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,51 @@
 class RoomsController < ApplicationController
+  # Loads:
+  # @rooms = all rooms
+  # @room = current room when applicable
+  before_action :load_entities
+
   def index
+    @rooms = Room.all
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new permitted_parameters
+
+    if @room.save
+      flash[:success] = "Room #{@room.name} was created successfully"
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @room.update_attributes(permitted_parameters)
+      flash[:success] = "Room #{@room.name} was updated successfully"
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def load_entities
+    @rooms = Room.all
+    @room = Room.find(params[:id]) if params[:id]
+  end
+
+  def permitted_parameters
+    params.require(:room).permit(:name)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,3 @@
 class Room < ApplicationRecord
+  has_many :room_messages, dependent: :destroy
 end
