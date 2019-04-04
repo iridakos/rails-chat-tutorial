c946ec324d466b1fdcf90ff41c1235e0f4134e9f
iridakos <9477868+iridakos@users.noreply.github.com>
2019-04-04 16:25:55 +0300

    Unique username

--- db/migrate/20190404100700_devise_create_users.rb
+++ db/migrate/20190404100700_devise_create_users.rb
@@ -38,6 +38,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     end
 
     add_index :users, :email,                unique: true
+    add_index :users,  :username,             unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_04_04_102347) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
