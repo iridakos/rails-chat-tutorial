84a3fc751ca330b2483c40a8ae4722529545d5f6
Lazarus Lazaridis <9477868+iridakos@users.noreply.github.com>
2019-04-13 01:55:23 +0300

    ack

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,4 @@
 class Room < ApplicationRecord
-  has_many :room_messages, dependent: :destroy
+  has_many :room_messages, dependent: :destroy,
+                           inverse_of: :room
 end
--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -1,5 +1,6 @@
 class RoomMessage < ApplicationRecord
-  belongs_to :room
+  belongs_to :room, inverse_of: :room_messages
+
   belongs_to :user
 
   def as_json(options)
